1cecdb5807dbe8d4bf3c2a782ae909bc0a0f0b5e
Dong_Kyu_Kim <dong.kyu.kim@vanderbilt.edu>
2019-01-14 10:58:32 -0600

    Summary_01142019
    Added updated +remmi and Denoise folders. Added process.m script for processing scan data. 'process_input.txt' contains inputs for process.m (i.e. input directory and scan numbers).

---
+++ matlab/+remmi/+vendors/bmatBruker.m
@@ -0,0 +1,101 @@
+function bm = bmatBruker(pp)
+
+
+dt = .004; % time resolution, ms
+gamma = 42.576; % MHz/T
+te = pp.te(1)*1000; % ms
+tm = 0; % ms
+gmax = pp.methpars.PVM_GradCalConst/gamma; % mT/m
+
+% # of B0 images
+nB0 = pp.methpars.PVM_DwAoImages; 
+nB0end = pp.methpars.REMMI_DwAoImagesEnd;
+nB0begin = nB0 - nB0end;
+
+ref = remmi.util.strsplit(pp.methpars.RefPulse1(2:end-1),',');
+refPulseDur = str2double(ref{1});
+tssr = 2*pp.methpars.REMMI_EddyDelay + refPulseDur; % ms
+gssr = pp.methpars.RefSliceGrad*gmax/100; % mT/m
+
+tcrush = pp.methpars.EncGradDur; % ms
+gcrush = pp.methpars.REMMI_ModSpoilerAmp(1)*gmax/100; % mT/m
+
+% obtain normalized diffusion directions
+dwdir = reshape(pp.methpars.PVM_DwDir,3,[]);
+dro = [zeros(1,nB0begin) dwdir(1,:) zeros(1,nB0end)];
+dpe = [zeros(1,nB0begin) -dwdir(2,:) zeros(1,nB0end)];
+dsl = [zeros(1,nB0begin) dwdir(3,:) zeros(1,nB0end)];
+
+if strcmpi(pp.methpars.REMMI_DwGradPolarity,'Yes')
+    dro = [dro, -dro];
+    dpe = [dpe, -dpe];
+    dsl = [dsl, -dsl];
+    
+    dro = repmat(dro,[1 pp.methpars.PVM_NRepetitions/2]);
+    dpe = repmat(dpe,[1 pp.methpars.PVM_NRepetitions/2]);
+    dsl = repmat(dsl,[1 pp.methpars.PVM_NRepetitions/2]);
+else
+    dro = repmat(dro,[1 pp.methpars.PVM_NRepetitions]);
+    dpe = repmat(dpe,[1 pp.methpars.PVM_NRepetitions]);
+    dsl = repmat(dsl,[1 pp.methpars.PVM_NRepetitions]);
+end
+
+% diffusion gradients
+gdiff = pp.methpars.PVM_DwGradAmp*gmax/100; % mT/m
+bigD = pp.methpars.PVM_DwGradSep; % ms
+litD = pp.methpars.PVM_DwGradDur; % ms
+
+for n=1:length(dro)
+    for p=1:length(gdiff)
+        t = 0:dt:(te+tm);
+        Gr = zeros(size(t));
+        Gp = zeros(size(t));
+        Gs = zeros(size(t));
+
+        % 180 slice select and crusher
+        mask = (t>te/2-tssr/2-tcrush) & (t<=te/2);
+        Gs(mask) = gcrush;
+        mask = (t>te/2-tssr/2) & (t<=te/2);
+        Gs(mask) = gssr;
+
+        mask = (t<te/2+tm+tssr/2+tcrush) & (t>=te/2+tm);
+        Gs(mask) = -gcrush;
+        mask = (t<te/2+tm+tssr/2) & (t>=te/2+tm);
+        Gs(mask) = -gssr;
+
+        % read out direction
+        mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
+        Gr(mask) = gdiff(p)*dro(n);
+        mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
+        Gr(mask) = -gdiff(p)*dro(n);
+
+        % phase encode direction
+        mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
+        Gp(mask) = +gdiff(p)*dpe(n);
+        mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
+        Gp(mask) = -gdiff(p)*dpe(n);
+
+        % slice select direction
+        mask = (t>te/2-bigD/2-litD/2+tm/2) & (t<te/2-bigD/2+litD/2+tm/2);
+        Gs(mask) = gdiff(p)*dsl(n);
+        mask = (t>te/2+bigD/2-litD/2+tm/2) & (t<te/2+bigD/2+litD/2+tm/2);
+        Gs(mask) = -gdiff(p)*dsl(n);
+
+        G = [Gr;Gp;Gs];
+
+        % plot(G')
+        % pause();
+
+        % calculate the b-matrix
+        k = cumsum(G,2)*dt*gamma*2*pi/1000; % 1/mm
+        b = zeros(3,3);
+        for m=1:size(k,2)
+            b = b + k(:,m)*k(:,m)';
+        end
+        %bm(:,:,n) = b*dt/1000; % s/mm^2
+
+        bm(:,(n-1)*length(gdiff)+p) = [diag(b);diag(b,-1);diag(b,-2);1000/dt]*dt/1000; % s/mm^2
+    end
+    
+end
+
---
+++ matlab/process.m
@@ -0,0 +1,160 @@
+function process(inputdir)
+% Process raw scan data to reconstruct images and calculate parameter maps
+%
+% Input
+%   inputdir=directory containing data on XNAT
+%
+% Output 
+%   Results are saved in inputdir/Output
+
+% Load in textfile with input parameters
+fid = fopen(fullfile(inputdir,'process_input.txt'),'r');
+if fid == -1
+    sprintf('Error: Could not open method file');
+end
+
+% Load text into cell array M
+% M{1}=variable names
+% M{2}=variable values
+M = textscan(fid,'%s %s',...
+'Delimiter','=');
+
+% List the study path and experiments to process
+info.spath = fullfile(inputdir,M{2}{contains(M{1},'filename')});
+
+% Directory to save
+outputdir = fullfile(inputdir,'Output');
+
+% Create save directory
+mkdir(fullfile(inputdir,'Output'));
+
+%% HRANAT
+if ~isempty(find(contains(M{1},'scannum.hranat')))
+    info.exps=str2num(M{2}{contains(M{1},'scannum.hranat')});
+    ws_hranat = remmi.workspace(fullfile(outputdir,'hranat.mat'));
+    
+    % Reconstruct the data
+    ws_hranat.images = remmi.recon(info);
+    
+    % Save image in NIFTI
+    niftiwrite(ws_hranat.images.img,fullfile(outputdir,'hranat.nii'));
+end
+
+%% MSE
+if ~isempty(find(contains(M{1},'scannum.mse')))
+    info.exps=str2num(M{2}{contains(M{1},'scannum.mse')});
+    ws_mse = remmi.workspace(fullfile(outputdir,'mse.mat'));
+    
+    % Reconstruct the data
+    ws_mse.images = remmi.recon(info);
+    
+    % Set a threshold mask
+    ws_mse.images = remmi.util.thresholdmask(ws_mse.images,.2);
+    dimages = ws_mse.images; % complex denoised
+    
+    % Size of window
+    Nv = ceil(size(ws_mse.images.img,4)^(1/3));
+    
+    % Denoise complex images
+    [dimages.img,~,~] = ...
+        denoiseCV(ws_mse.images.img,[Nv,Nv,Nv],ws_mse.images.mask);
+    
+    % Load denoised images into the remmi workspace, then clear dummies
+    ws_mse.dimages = dimages;
+    clear dimages
+    
+    % Multi-exponential T2 Analysis using MERA
+    [metrics,fitting,analysis]= remmi.mse.mT2options;
+    metrics = rmfield(metrics,'gmT2');
+    metrics = rmfield(metrics,'MWF');
+    % Fitting parameters
+    fitting.B1fit = 'y';
+    fitting.regtyp = 'mc';
+    fitting.regweight = 1e-1;
+    fitting.regadj = 'manual';
+    fitting.numberT = 100;
+    fitting.rangeT = [ws_mse.images.pars.te(1)*.75,ws_mse.images.pars.te(end)*4/3];
+    
+    fitting.rangetheta = [135 180]; % normally use 135,180, but B1 is close to 180
+    fitting.numbertheta = 10;
+    fitting.fixedT1 = .2; % based on previous IR anlaysis
+    analysis.tolextract = 0;
+    
+    % Specify output metrics
+    metrics.S = @(out) out.S;
+    % metrics.T = @(out) out.T;
+    % metrics.SNR = @(out) out.SNR;
+    % metrics.Tv = @(out) out.Tv;
+    % metrics.Fv = @(out) out.Fv;
+    metrics.B1 = @(out) out.theta;
+    %metrics.MWF  = ...
+    % str2func('@(out) sum(out.Fv.*(out.Tv>0.005 & out.Tv<.027),1)./sum(out.Fv,1)');
+    metrics.MWF2  = ...
+        str2func('@(out) sum(out.S(8:47,:))./sum(out.S(8:end,:))');
+    
+    ws_mse.T2spect= remmi.mse.mT2(ws_mse.dimages,metrics,fitting,analysis); 
+    
+    % mse.dT2spect saves to current directory if mse_full.mat is too large
+    % move this to the appropriate directory
+    try
+        movefile('mse.T2spect.mat',fullfile(outputdir,'mse.T2spect.mat'))
+    catch
+        warning('mse.T2spect.mat is in MSE.mat');
+    end
+end
+
+%% SIR
+if ~isempty(find(contains(M{1},'scannum.sir')))
+    info.exps=str2num(M{2}{contains(M{1},'scannum.sir')});
+    ws_sir = remmi.workspace(fullfile(outputdir,'sir.mat'));
+    
+    % Reconstruct the data
+    ws_sir.images = remmi.recon(info);
+    
+    % Set a threshold mask
+    ws_sir.images = remmi.util.thresholdmask(ws_sir.images);
+    dimages = ws_sir.images; % complex denoised
+    
+    % Size of window
+    Nv = ceil(size(ws_sir.images.img,4)^(1/3));
+    
+    % Denoise complex images
+    [dimages.img,~,~] = denoiseCV(ws_sir.images.img,[Nv,Nv,Nv],...
+        ws_sir.images.mask);
+    
+    % Load denoised images into the remmi workspace, then clear dummies
+    ws_sir.dimages = dimages;
+    clear dimages
+    
+    % Perform qMT analysis
+    ws_sir.qMT = remmi.ir.qmt(ws_sir.dimages); 
+    
+    % Convert maps to NIFTI
+    niftiwrite(ws_sir.qMT.BPF,fullfile(outputdir,'BPF.nii'));
+    niftiwrite(ws_sir.qMT.T1,fullfile(outputdir,'T1.nii'));
+    niftiwrite(ws_sir.qMT.inv_eff,fullfile(outputdir,'inv_eff.nii'));
+end
+
+%% DTI
+if ~isempty(find(contains(M{1},'scannum.dti')))
+    info.exps=str2num(M{2}{contains(M{1},'scannum.dti')});
+    ws_dti = remmi.workspace(fullfile(outputdir,'dti.mat'));
+    
+    %Reconstruct the data
+    ws_dti.images = remmi.recon(info);
+    
+    % Set a threshold mask
+    ws_dti.images = remmi.util.thresholdmask(ws_dti.images);
+    
+    % Add b matrix
+    ws_dti.images = remmi.dwi.addbmatrix(ws_dti.images);
+    
+    % Perform DTI analysis
+    ws_dti.dti = remmi.dwi.dti(ws_dti.images);
+    
+    % Convert maps to NIFTI
+    niftiwrite(ws_dti.dti.fa,fullfile(outputdir,'FA.nii'));
+    niftiwrite(ws_dti.dti.adc,fullfile(outputdir,'ADC.nii'));
+end
+
+
