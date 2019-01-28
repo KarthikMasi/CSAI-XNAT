d6dc07a6e7014ee2ad6afee469a594a5e0f53a4e
ramadak <ramadak@masi-2.ds.vanderbilt.edu>
2019-01-28 15:50:02 -0600

    Made changes to outputs after 4 brain segmentation

--- spiders/Spider_full_remmi_process_test.py
+++ spiders/Spider_full_remmi_process_test.py
@@ -41,9 +41,11 @@ process(scandata,'$temp_dir')
 
 % Move files
 system(['mkdir ' fullfile('$temp_dir','RESULTS')]);
-system(['mv ' fullfile('$temp_dir','Output','*.mat') ' ' fullfile('$temp_dir','RESULTS')])
-system(['mv ' fullfile('$temp_dir','Output','*.nii') ' ' fullfile('$temp_dir','RESULTS')])
-system(['mv ' fullfile('$temp_dir','Output','*.pdf') ' ' fullfile('$temp_dir','RESULTS')])
+system(['mv ' fullfile('$temp_dir','results','Full_Data') ' ' fullfile('$temp_dir','RESULTS')])
+system(['mv ' fullfile('$temp_dir','results','Parameter_Maps') ' ' fullfile('$temp_dir','RESULTS')])
+system(['mv ' fullfile('$temp_dir','results','Split_Data') ' ' fullfile('$temp_dir','RESULTS')])
+system(['mv ' fullfile('$temp_dir','results','Interp') ' ' fullfile('$temp_dir','RESULTS')])
+system(['mv ' fullfile('$temp_dir','results','*.pdf') ' ' fullfile('$temp_dir','RESULTS')])
 
 """
 
