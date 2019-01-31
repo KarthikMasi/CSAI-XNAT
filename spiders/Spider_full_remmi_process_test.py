0e7dc86a65254243a9288c91443a1d7497dedf99
ramadak <ramadak@masi-2.ds.vanderbilt.edu>
2019-01-31 09:51:32 -0600

    Directory structure updated

--- spiders/Spider_full_remmi_process_test.py
+++ spiders/Spider_full_remmi_process_test.py
@@ -45,8 +45,6 @@ system(['mv ' fullfile('$temp_dir','Full_Data') ' ' fullfile('$temp_dir','RESULT
 system(['mv ' fullfile('$temp_dir','Parameter_Maps') ' ' fullfile('$temp_dir','RESULTS')])
 system(['mv ' fullfile('$temp_dir','Split_Data') ' ' fullfile('$temp_dir','RESULTS')])
 system(['mv ' fullfile('$temp_dir','Interp') ' ' fullfile('$temp_dir','RESULTS')])
-system(['mv ' fullfile('$temp_dir','*.pdf') ' ' fullfile('$temp_dir','RESULTS')])
-
 """
 
 if __name__ == '__main__':
