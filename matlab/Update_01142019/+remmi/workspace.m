fd30947bf5a6b0fa36e5ebcbcacef3436421bbb4
Dong_Kyu_Kim <dong.kyu.kim@vanderbilt.edu>
2019-02-06 15:42:14 -0600

    Update_02062019
    removed 'first character of "name" must be a letter' exception

--- matlab/Update_01142019/+remmi/workspace.m
+++ matlab/Update_01142019/+remmi/workspace.m
@@ -33,11 +33,11 @@ classdef workspace < dynamicprops
             if exist(fname,'var') || isempty(fname)
                 error('workspace() requires a filename');
             elseif ischar(fname)
-                if ~isstrprop(fname(1),'alpha')
-                    error('The first character of "name" must be a letter');
-                else
+%                 if ~isstrprop(fname(1),'alpha')
+%                     error('The first character of "name" must be a letter');
+%                 else
                     obj.filename = fname;
-                end
+%                 end
             else
                 error('"fname" must be a character string');
             end
