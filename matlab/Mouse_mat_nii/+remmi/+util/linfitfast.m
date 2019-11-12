c53ab7af91d1b2de215b545941df3e70401ae38e
Dong Kyu Kim <dong.kyu.kim@vanderbilt.edu>
2019-11-12 11:28:30 -0600

    Cleaned up old directories

--- matlab/Mouse_mat_nii/+remmi/+util/linfitfast.m
+++
@@ -1,4 +0,0 @@
-function par = linfitfast(x,y,w)
-
-M = [x, ones(size(x))];
-par = (M'*diag(w)*M)\(M'*diag(w)*y);
--- matlab/Mouse_mat_nii/+remmi/dataset.m
+++
@@ -1,7 +0,0 @@
-function ws = dataset(varargin)
-
-warning(['remmi.dataset has been renamed to remmi.workspace, ' ...
-         'and will be depreciated in later version. Please update ' ...
-         'your code to use remmi.workspace()']);
-
-ws = remmi.workspace(varargin{:});
